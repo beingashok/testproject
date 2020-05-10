151db689656aa5fba15f7404ffe6dbfbda1eab77
Ashok kumar Thiagarajan <ashokkumarthiagarajan@Ashoks-MacBook-Pro.local>
2020-05-10 09:22:25 +0100

    skeleton

---
+++ aws-serverless-workshops-master/WebApplication/1_StaticWebHosting/website/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-2_hRAB121rC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'ed5pi9ee0j4p1u93fmjfv7a50', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://91hbledm7j.execute-api.eu-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
